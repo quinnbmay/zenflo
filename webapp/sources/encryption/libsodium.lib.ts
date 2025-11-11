a309de4d14f6f8725029d303acc95d7cbc394428
May Marketing SEO <quinn@maymarketingseo.com>
2025-11-10 20:27:06 -0500

    debug: Add logging to identify which libsodium module is loaded

--- webapp/sources/encryption/libsodium.lib.ts
+++ webapp/sources/encryption/libsodium.lib.ts
@@ -1,2 +1,6 @@
 import sodium from '@more-tech/react-native-libsodium';
+
+console.log('[libsodium.lib.native] Loading @more-tech/react-native-libsodium for native platform');
+console.log('[libsodium.lib.native] Loaded:', typeof sodium);
+
 export default sodium;
--- webapp/sources/encryption/libsodium.lib.web.ts
+++ webapp/sources/encryption/libsodium.lib.web.ts
@@ -1,5 +1,9 @@
 // Use dynamic import to ensure the module is properly loaded
 const _sodium = require('libsodium-wrappers');
 
+console.log('[libsodium.lib.web] Loading libsodium-wrappers for web platform');
+console.log('[libsodium.lib.web] Has ready:', 'ready' in _sodium);
+console.log('[libsodium.lib.web] Has crypto_box_keypair:', 'crypto_box_keypair' in _sodium);
+
 // Re-export the entire module to preserve all properties and constants
 export default _sodium;
